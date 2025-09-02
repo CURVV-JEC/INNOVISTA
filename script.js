4165ac086cd78091e52e5cbc116a54d1e9b6f9bd
CURVV-JEC <curvv.jecjabalpur@gmail.com>
2025-09-03 04:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
   })
-   // Registration Form
+  // Registration Form
   document.getElementById("registration-form").addEventListener("submit", function (e) {
     e.preventDefault() // prevent page reload
 
